6dd1e36109ca8c9b879e6d3c365d2f6b3780f0b9
Luis-RodriguezB <lrodriguezbaltodano97@gmail.com>
2022-05-13 20:02:26 -0600

    nav-links

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-// const RICK_API = 'https://rickandmortyapi.com/api'
 
-// const getData = async ( type = 'character', amount = 1 ) => {
-//     const response = await fetch(`${RICK_API}/${type}/${amount}`);
-//     const data = await response.json();
-
-//     return data
-// }
-
-// getData().then( data => console.log(data))
+function active({ target }) {
+    document.querySelectorAll('button').forEach( button => {
+        delete button.dataset.active;
+    });
+    target.dataset.active = true;
+}
