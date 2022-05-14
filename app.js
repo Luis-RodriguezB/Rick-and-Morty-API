dad1a56f58db1d078483746a31161617b33ae330
Luis-RodriguezB <lrodriguezbaltodano97@gmail.com>
2022-05-13 20:44:01 -0600

    add comment function

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 
+/* 
+En la funcion viene event (ver en el html ) 
+se hace un destructuring "{ target }" para obtener el target con los valores del button clickeado
+*/
 function activeLink({ target }) {
     document.querySelectorAll('button').forEach( button => {
         delete button.dataset.active;
