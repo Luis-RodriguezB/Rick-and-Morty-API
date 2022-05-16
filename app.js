d1c579824fcf767d283d7b1910cb05d9c99fd81f
luis.e.rodriguez <luis.e.rodriguez@accenture.com>
2022-05-16 17:17:03 -0600

    ....

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const API = `https://rickandmortyapi.com/api`;
 const cardList = document.getElementById('cards-container');
+let arr = [];
 
 
 function activeLink({ target }) {
@@ -9,3 +10,17 @@ function activeLink({ target }) {
     target.dataset.active = true;
 }
 
+async function hello() {
+    arr = [];
+    await fetch(`${API}/character`)
+        .then( resp => resp.json())
+        .then( data => {
+            arr = data.results;
+            arr.forEach( item => {
+                card = `
+                
+                `;
+            });
+        })
+        .catch( error => console.log(`Error: ${error}`));
+}
