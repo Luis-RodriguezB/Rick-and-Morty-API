fad7afe168d5cc1804673cbf211e6cee21f6add0
luis.e.rodriguez <luis.e.rodriguez@accenture.com>
2022-05-16 17:07:46 -0600

    add api

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+const API = `https://rickandmortyapi.com/api`;
+const cardList = document.getElementById('cards-container');
 
-/* 
-En la funcion viene event (ver en el html ) 
-se hace un destructuring "{ target }" para obtener el target con los valores del button clickeado
-*/
+
 function activeLink({ target }) {
     document.querySelectorAll('button').forEach( button => {
         delete button.dataset.active;
     });
     target.dataset.active = true;
 }
+
